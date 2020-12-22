47c313b6f42830d1da8824d0875cd9a6dc3162bd
Zahid Khan <zahid.khan846@hotmail.com>
2020-12-22 22:31:33 +0530

    final auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const sgMail = require("@sendgrid/mail");
 
+const crypto = require("crypto");
+
 sgMail.setApiKey(
   "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"
 );
@@ -110,4 +112,106 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect("/");
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset-password", {
+    path: "/reset-password",
+    pageTitle: "reset-password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset-password");
+    }
+    const token = buffer.toString("hex");
+
+    const userEmail = req.body.email;
+
+    User.findOne({ email: userEmail })
+      .then((user) => {
+        if (!user) {
+          req.flash("no user fould related to ur query");
+          return res.redirect("/reset-password");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(() => {
+        res.redirect("/");
+        sgMail.send({
+          to: userEmail,
+          from: "zahid.khan846@hotmail.com",
+          subject: "Sending reset link",
+          html: `
+          <p>Reset link:</p>
+          <p><a href='http://localhost:3000/reset-password/${token}'>Link</a></p>
+          `,
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "new-password",
+        userId: user._id.toString(),
+        userToken: token,
+        errorMessage: message,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const userId = req.body.userId;
+  const userToken = req.body.userToken;
+  const newPassword = req.body.password;
+
+  let resetUser;
+
+  User.findOne({
+    resetToken: userToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -23,6 +23,8 @@ const userSchema = new Schema({
       },
     ],
   },
+  resetToken: String,
+  resetTokenExpiration: Date,
 });
 
 userSchema.methods.addToCart = function (product) {
